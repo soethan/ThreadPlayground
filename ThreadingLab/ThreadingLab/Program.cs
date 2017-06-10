07031303b58cb00afe302b36888f689a14f93ade
Soe Than <soethan@gmail.com>
2017-06-10 21:41:34 +0800

    init

---
+++ ThreadingLab/ThreadingLab/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThreadingLab.Models;
+
+namespace ThreadingLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task t1 = new Task(new Action<object>(printMessage), new Person { Id = 1, Name = "Steven" });
+            t1.Start();
+
+            Console.Read();
+        }
+
+        private static void printMessage(object obj)
+        {
+            var person = obj as Person;
+            Console.WriteLine(string.Format("Id={0};Name={1};", person.Id.ToString(), person.Name));
+        }
+    }
+}
